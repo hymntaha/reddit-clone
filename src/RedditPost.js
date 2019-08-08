2a4284e8b0556fcf9d1ae4033df3d607254dcbd7
taha uygun <tahauygun@gmail.com>
2019-08-07 23:39:57 -0700

    adding styles

--- src/RedditPost.js
+++ src/RedditPost.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+const style = {display:'flex', flexDirection:'column'}
+
 export default () => {
   return (
-    <div>
+    <div style={style}>
       <a href="http://google.com">My buddy's dog saw a moose</a>
-      <p>
-        580 Comments
-      </p>
-      <p>
-        Submitted 5 minutes ago
-      </p>
+      <div>
+        <p>
+          580 Comments
+        </p>
+        <p>
+          Submitted 5 minutes ago
+        </p>
+      </div>
     </div>
   )
 }
