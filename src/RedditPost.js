1409c86b6fab834a3119d21a5ebaf7cdd55a3871
taha uygun <tahauygun@gmail.com>
2019-08-08 22:38:38 -0700

    fxing some stuff

--- src/RedditPost.js
+++ src/RedditPost.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './RedditPost.css';
+import styles from './RedditPost.css';
 
 export default () => {
   return (
-    <div className='redditPostContainer'>
+    <div className={styles.redditPostContainer}>
       <a href="http://google.com">My buddy's dog saw a moose</a>
-      <div className="redditPostBottomContainer">
-        <p  className="comments">
+      <div className={styles.redditPostBottomContainer}>
+        <p  className={styles.comments}>
           580 Comments
         </p>
         <p>
